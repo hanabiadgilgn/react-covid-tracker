b82ba6f471f189bd1838fbc59a52c0b57ec3f531
Hana <hanabia2014@gmail.com>
2021-01-05 10:04:09 -0600

    First Commit

---
+++ build/precache-manifest.378d730b3a4e24366ed2f1a8398a319c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ec45b15723a232fcaae19827c6de29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "8a34d1925f16fcbbad15",
+    "url": "/static/css/main.b068b1a8.chunk.css"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/js/2.c566785b.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.c566785b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a34d1925f16fcbbad15",
+    "url": "/static/js/main.b0545b33.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
